9dbecb53fa26e34400383ae83296c070c46608a6
xingchen121 <lu1935930389@gmail.com>
2022-03-06 16:41:06 +0800

    blog

---
+++ blog-api/src/main/java/top/naccl/BlogApiApplication.java
@@ -0,0 +1,16 @@
+package top.naccl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lu193
+ */
+@SpringBootApplication
+public class BlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiApplication.class, args);
+	}
+
+}
